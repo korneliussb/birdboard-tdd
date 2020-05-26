814b5684a1f9141f2f1271fdd7a9538976b70595
korneliussb <korneliussb@student.ce.undip.ac.id>
2020-05-26 11:38:01 +0700

    Create Project View

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Birdboard</title>
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
+</head>
+
+<body>
+
+    <form method="POST" action="/projects" class="container" style="padding-top: 40px">
+        @csrf
+
+        <h1 class="heading is-1">Create a project</h1>
+
+        <div class="field">
+            <label class="label" for="title">Title</label>
+
+            <div class="control">
+                <input type="text" class="input" name="title" placeholder="Title">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label" for="description">Description</label>
+
+            <div class="control">
+                <textarea name="description" class="textarea" id="" cols="30" rows="10"></textarea>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">Create Project</button>
+            </div>
+        </div>
+
+
+    </form>
+
+</body>
+
+</html>
--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -6,32 +6,22 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ProjectsTest extends TestCase
+class ManageProjectsTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
     /** @test */
-    public function guests_cannot_create_projects()
+    public function guests_cannot_manage_projects()
     {
         // $this->withoutExceptionHandling();
-
-        $attributes = factory('App\Project')->raw(); // store as an array, not an object => raw()
-
-        $this->post('/projects', $attributes)->assertRedirect('login');
-    }
-
-    /** @test */
-    public function guest_cannot_view_projects()
-    {
-        $this->get('/projects')->assertRedirect('login');
-    }
-
-    /** @test */
-    public function guest_cannot_view_a_single_project()
-    {
         $project = factory('App\Project')->create();
 
+        // $attributes = factory('App\Project')->raw(); // store as an array, not an object => raw()
+
+        $this->get('/projects')->assertRedirect('login');
+        $this->get('/projects/create')->assertRedirect('login');
         $this->get($project->path())->assertRedirect('login');
+        $this->post('/projects', $project->toArray())->assertRedirect('login');
     }
 
     /** @test */
@@ -40,6 +30,8 @@ class ProjectsTest extends TestCase
         $this->withoutExceptionHandling();
 
         $this->actingAs(factory('App\User')->create());
+
+        $this->get('projects/create')->assertStatus(200);
 
         $attributes = [
             'title' => $this->faker->sentence,
