e13e593561c32d46afba45e9b0e600b134498578
korneliussb <korneliussb@student.ce.undip.ac.id>
2020-06-02 00:54:26 +0700

    34 Validation Errors for Multiple Forms

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,4 +1,4 @@
-<div class="card" style="height: 200px">
+<div class="card flex flex-col" style="height: 200px">
 
     <h3 class="font-normal text-lg py-4 mb-3 -ml-5 border-l-4 border-blue-light pl-4">
         <a href="{{ $project->path() }}" class="text-black no-underline">
@@ -7,7 +7,7 @@
         </a>
     </h3>
 
-    <div class="text-grey mb-4">{{ str_limit($project->description, 150) }}</div>
+    <div class="text-grey mb-4 flex-1">{{ str_limit($project->description, 150) }}</div>
     <!-- <div>{{ Illuminate\Support\Str::limit($project->description, 200) }}</div> -->
 
     <footer>
