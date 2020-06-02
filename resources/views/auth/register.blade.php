a8dc6526fdb55354fe4f94718973a9fa01e1ceb5
korneliussb <korneliussb@student.ce.undip.ac.id>
2020-06-02 13:11:23 +0700

    36 Light and Dark Themes with Tailwinds

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('register') }}" class="lg:w-1/2 lg:mx-auto bg-white py-12 px-16 rounded shadow">
+<form method="POST" action="{{ route('register') }}" class="lg:w-1/2 lg:mx-auto bg-card py-12 px-16 rounded shadow">
     @csrf
 
     <h1 class="text-2xl font-normal mb-10 text-center">Register</h1>
@@ -10,7 +10,7 @@
         <label class="label text-sm mb-2 block" for="name">Name</label>
 
         <div class="control">
-            <input id="name" type="text" class="input bg-transparent border border-grey-light rounded p-2 text-xs w-full{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+            <input id="name" type="text" class="input bg-transparent border border-muted-light rounded p-2 text-xs w-full{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
         </div>
     </div>
 
@@ -18,7 +18,7 @@
         <label class="label text-sm mb-2 block" for="email">Email Address</label>
 
         <div class="control">
-            <input id="email" type="email" class="input bg-transparent border border-grey-light rounded p-2 text-xs w-full{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+            <input id="email" type="email" class="input bg-transparent border border-muted-light rounded p-2 text-xs w-full{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
         </div>
     </div>
 
@@ -26,7 +26,7 @@
         <label class="label text-sm mb-2 block" for="password">Password</label>
 
         <div class="control">
-            <input id="password" type="password" class="input bg-transparent border border-grey-light rounded p-2 text-xs w-full{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+            <input id="password" type="password" class="input bg-transparent border border-muted-light rounded p-2 text-xs w-full{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
         </div>
     </div>
 
@@ -34,7 +34,7 @@
         <label class="label text-sm mb-2 block" for="password-confirmation">Confirm Password</label>
 
         <div class="control">
-            <input id="password-confirmation" type="password" class="input bg-transparent border border-grey-light rounded p-2 text-xs w-full" name="password_confirmation" required>
+            <input id="password-confirmation" type="password" class="input bg-transparent border border-muted-light rounded p-2 text-xs w-full" name="password_confirmation" required>
         </div>
     </div>
 
